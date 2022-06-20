01e48ebb88c11c07d53349bd5007cc27f07916d7
Benjamin <bstanf@uw.edu>
2022-06-20 16:20:26 -0700

    realization I used the arrays for the comnparissons...

--- script/script.js
+++ script/script.js
@@ -14,9 +14,9 @@ var generateBtn = document.querySelector("#generate");
 
 
 // Write password to the #password input
-function writePassword() {
+function generatePassword() {
   passwordLength = prompt("Enter a number between 8 and 128");
-// Validating that any value has been entered and if it meets the criteria.
+// Validating that any value has been entered for password length and if it meets the criteria.
   if (!passwordLength) {
     alert("You need to enter a value.");
   } else if (passwordLength < 8 || passwordLength > 128) {
@@ -28,28 +28,50 @@ function writePassword() {
     lowerCaseBoolean = confirm("Do you want to use lowecase letters?");
   };
 
-  // Validate that at least 1 variation was selected
+  // Validate that at least 1 variation was selected as follows:
+  // Any choices selected:
   if (!numbersBoolean && !specialBoolean && !upperCaseBoolean && !lowerCaseBoolean) {
       characterArray = alert("No criteria selected.");
-  } else if (numbersBoolean && specialChar && upperCaseBoolean && lowerCaseBoolean) {
+  } 
+  //  All four choices selected:
+  else if (numbersBoolean && specialChar && upperCaseBoolean && lowerCaseBoolean) {
     characterArray = numbers.concat(specialChar, upperCase, lowerCase);
-  }  else if (specialChar && numbers && lowerCase) {
+  }  
+  // Selections of 3 arrays:
+  else if (specialChar && numbers && lowerCase) {
     characterArray = specialChar.concat(numbers, lowerCase);
   } else if (specialChar && numbers && upperCase) {
     characterArray = specialChar.concat(numbers, upperCase);
-  }else if (specialChar && numbers && upperCase) {
-    characterArray = specialChar.concat(numbers, upperCase);
-  }else if (specialChar && numbers && upperCase) {
-    characterArray = specialChar.concat(numbers, upperCase);
-  }else if (specialChar && numbers && upperCase) {
-    characterArray = specialChar.concat(numbers, upperCase);
-  }else if (specialChar && numbers && upperCase) {
-    characterArray = specialChar.concat(numbers, upperCase);
-  }else if (specialChar && numbers && upperCase) {
-    characterArray = specialChar.concat(numbers, upperCase);
-  }else if (specialChar && numbers && upperCase) {
-    characterArray = specialChar.concat(numbers, upperCase);
+  }else if (numbers && upperCase && lowerCase) {
+    characterArray = numbers.concat(upperCase, lowerCase);
+  }else if (specialChar && upperCase && lowerCase) {
+    characterArray = specialChar.concat(upperCase, lowerCase);
   }
+  // Selections of 2 arrays:
+  else if (upperCase && lowerCase) {
+    characterArray = upperCase.concat(lowerCase);
+  }else if (lowerCase && numbers) {
+    characterArray = lowerCase.concat(numbers);
+  }else if (upperCase && numbers) {
+    characterArray = upperCase.concat(numbers);
+  } else if (specialChar && lowerCase) {
+    characterArray = specialChar.concat(lowerCase);
+  } else if (specialChar && upperCase) {
+    characterArray = specialChar.concat(upperCase);
+  } else if (specialChar && numbers) {
+    characterArray = specialChar.concat(numbers);
+  }
+  // Single selections:
+  else if (specialChar) {
+    characterArray = specialChar;
+  } else if (numbers) {
+    characterArray = numbers;
+  } else if (upperCase) {
+    characterArray = upperCase;
+  } else if (lowerCase) {
+    characterArray = lowerCase;
+  }
+  console.log(characterArray);
   
   // var password = generatePassword();
   // var passwordText = document.querySelector("#password");
@@ -59,4 +81,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
